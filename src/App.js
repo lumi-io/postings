52b095b67b870e1974f5a6e60ec1d381a42f6009
yoontsy2000 <yoontsy2000@gmail.com>
2020-12-24 10:10:55 +0900

    Added react router to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './components/Dashboard'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Dashboard from './components/Dashboard';
+import Form from './components/Form';
+import Login from './components/Login';
+import SideMenu from './components/SideMenu';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard/>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/form" component={Form} />
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'; 
+import clsx from 'clsx';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <Typography>Apply for this position</Typography>
+      </div>
+    )
+  }
+}
+
+export default Form;
