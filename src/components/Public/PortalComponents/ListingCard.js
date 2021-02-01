b3f2ef1339cf2d8a9d87a6f8487ae94b3db27d97
Jin Young Bang <jybang@bu.edu>
2021-01-31 20:40:18 -0500

    Created public listings card components

---
+++ src/components/Public/PortalComponents/ListingCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function OutlinedCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          {props.title}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Link to={"/portal/" + props.id}>
+        <Button size="small">Apply</Button>
+        </Link>
+      </CardActions>
+    </Card>
+  );
+}
