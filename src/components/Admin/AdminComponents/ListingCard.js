d6ed56b083e02f9dfa9ceb00bfeb14cb25662bce
Jin Young Bang <jybang@bu.edu>
2021-02-16 22:44:49 -0500

    Fixed link bug on sidebar and reconfigured links on listings dashboard

--- src/components/Admin/AdminComponents/ListingCard.js
+++ src/components/Admin/AdminComponents/ListingCard.js
@@ -3,11 +3,11 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 import styled from 'styled-components';
 
@@ -38,15 +38,19 @@ export default function ListingCard(props) {
           </CardContent>
         </CustomCardActionArea>
         <CustomCardActions>
-          <Button size="small" onClick={() => window.location.href = "/admin/listing/" + props.id}>
-            Edit
-        </Button>
-          <Button size="small" onClick={() => window.location.href = "/admin/listing/" + props.id + "/applicant"}>
+          <Link to={"/admin/listing/" + props.id} style={{ textDecoration: 'none' }}>
+            <Button>
+                Edit
+            </Button>
+          </Link>
+          <Link to={"/admin/listing/" + props.id + "/applicant"} style={{ textDecoration: 'none' }}>
+          <Button>
             View Applicants
-        </Button>
-          <Button size="small" onClick={deleteListing}>
+          </Button>
+          </Link>
+          <Button onClick={deleteListing}>
             Delete
-        </Button>
+         </Button>
         </CustomCardActions>
       </Card>
     </CardBorder>
