e9a942b9ccad473d1182284dfb980c503143e60a
Jin Young Bang <jybang@bu.edu>
2021-02-18 23:28:17 -0500

    login component created

--- src/components/Public/Login.js
+++ src/components/Public/Login.js
@@ -1,134 +1,163 @@
-import React, {Component} from 'react'; 
-import { withStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types'
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React, { useState } from 'react';
+import styled from 'styled-components'
+import axios from 'axios';
+import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { Redirect, Route } from "react-router";
 
-import BoardsAPI from '../../data/BoardsAPI'
 
-const styles = theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-});
+const Login = () => {
+    const classes = useStyles();
 
-class Login extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            loginParameters: {
-                emailAddress: '',
-                password: ''
-            }        
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: ""
+    });
+    const [error, setError] = useState("");
+
+    const handleTextChange = (e) => {
+        const { name, value } = e.target;
+        setUserInfo(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    const login = () => {
+        if (userInfo["email"] === "" || userInfo["password"] === "") {
+            setError("Fields are required.")
+            return;
         }
+        axios.post(
+            'http://127.0.0.1:5000/admin/login',
+            userInfo,
+            { headers: { 'Content-Type': 'application/json' } }
+        ).then((res) => {
+            console.log(res)
+            return <Redirect to={{ pathname: '/admin/listings', state: {} }} />
+        })
     }
 
-    handleSubmit(loginParameters) {
-      const {emailAddress, password} = loginParameters;
-        BoardsAPI.login(emailAddress, password);
-    }
-
-    handleFormChange(event) {
-        const {id, value} = event.target;
-        const newLoginParameters = Object.assign({}, this.state.loginParameters, {[id]: value});
-        this.setState({loginParameters: newLoginParameters})
-    }
-
-    render() {
-        const {emailAddress, password} = this.state.loginParameters;
-        const {classes} = this.props;
-
-        return (
-            <Container component="main" maxWidth="xs">
-              <CssBaseline />
-              <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                  <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                  Sign in
-                </Typography>
-                <form className={classes.form} noValidate>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="emailAddress"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                    value={emailAddress}
-                    onChange={event=>this.handleFormChange(event)}
-                  />
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={this.handleFormChange}
-                  />
-                  <Button
-                    type="button"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                    onClick={()=> {this.handleSubmit()}}
-                  >
-                    Sign In
-                  </Button>
-                  <Grid container>
+    return (
+        <BackgroundContainer>
+            <CustomContainer>
+                <form className={classes.form} noValidate autoComplete="off">
+                    <TitleText>lumi</TitleText>
+                    <CustomTextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="emailAddress"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        value={userInfo["email"]}
+                        onChange={handleTextChange}
+                    />
+                    <CustomTextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={userInfo["password"]}
+                        onChange={handleTextChange}
+                    />
+                    <CustomButton
+                        type="button"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={login}
+                    >
+                        Sign In
+                  </CustomButton>
+                </form>
+                <br></br>
+                <Grid container>
                     <Grid item xs>
-                      <Link href="#" variant="body2">
-                        Forgot password?
+                        <Link href="#" variant="body2">
+                            Placeholder for forgot password...?
                       </Link>
                     </Grid>
                     <Grid item>
-                      <Link href="#" variant="body2">
-                        {"Don't have an account? Sign Up"}
-                      </Link>
+                        <Link href="/signup" variant="body2">
+                            {"Don't have an account? Sign Up"}
+                        </Link>
                     </Grid>
-                  </Grid>
-                </form>
-              </div>
-            </Container>
-          );
+                </Grid>
+            </CustomContainer>
+        </BackgroundContainer>
+    )
+
+}
+
+const BackgroundContainer = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color:#F9F6F9;
+`;
+
+const TitleText = styled.div`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 72px;
+    line-height: 84px;
+    margin-bottom: 20px;
+`;
+
+const CustomTextField = withStyles({
+    root: {
+        'border-color': "red !important"
+    },
+})(TextField);
+
+const CustomContainer = withStyles({
+    root: {
+        // "background-color": "red",
+        "position": 'absolute',
+        "left": '50%',
+        "top": '50%',
+        "transform": 'translate(-50%, -50%)',
+        "padding-right": "10%",
+        "padding-left": "10%",
     }
- 
-}
+})(Container);
 
-Login.propTypes = {
-    classes: PropTypes.object.isRequired,
-    theme: PropTypes.object.isRequired
-}
+const CustomButton = withStyles({
+    root: {
+        "background-color": "#873CA2",
+        "color": "#F9F6F9",
+        "margin-top": "20px",
+        "padding": "10px",
+        '&:hover': {
+            "background-color": "#a355c0",
+            color: '#FFF'
+        }
+    }
+})(Button);
 
-export default withStyles(styles, {withTheme: true}) (Login);
+
+const useStyles = makeStyles((theme) => ({
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+}));
+
+export default Login;
