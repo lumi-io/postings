405efe9b461cbe800eaf2b0c37ab0f6234f313f5
Jin Young Bang <jybang@bu.edu>
2021-07-25 15:51:25 -0400

    Fixed build error

--- src/components/Public/Portal/Portal.js
+++ src/components/Public/Portal/Portal.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Helmet from "react-helmet";
 
 import ListingCard from "./components/ListingCard";
 import BackgroundOverlay from "../../BackgroundOverlay";
